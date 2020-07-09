ac446633b3c75cfb94c61c8cc1ffbfeb617a6fc6
MisterRnobe <mr.rnobe@gmail.com>
2020-07-09 22:53:43 +0300

    Main page implementation

---
+++ scripts/api-clients.js
@@ -0,0 +1,108 @@
+class RestaurantClient {
+
+    constructor(apiKey = '') {
+        this.baseUrl = `http://exam-2020-1-api.std-900.ist.mospolytech.ru/api/data1`;
+        this.apiKey = apiKey;
+    }
+
+    get = (page = 0, limit = 5, filter = () => true) => {
+        return fetch(`${this.baseUrl}?api_key=${this.apiKey}`)
+            .then(body => body.json())
+            .then(body => body.filter(elem => filter(elem)))
+            .then(filtered => filtered.sort((a, b) => b.rate - a.rate))
+            .then(filtered => filtered.slice(page * limit, (page + 1) * limit));
+    }
+
+    getById = (id) => {
+        return fetch(`${this.baseUrl}/${id}?api_key=${this.apiKey}`)
+            .then(body => body.json());
+    }
+
+
+}
+
+class DishClientStub {
+
+    dishes = [
+        {
+            id: 1,
+            name: 'Шакшука',
+            description: 'Блюдо из яиц, жаренных в соусе из помидоров, острого перца, лукa и приправ',
+            photoLink: 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-13.jpg'
+        },
+        {
+            id: 2,
+            name: 'Гамбургер',
+            description: 'Рубленый бифштекс из натуральной цельной говядины на карамелизованной булочке, заправленной горчицей, кетчупом, луком и кусочком маринованного огурчика.',
+            photoLink: 'https://img03.rl0.ru/eda/c620x415i/s1.eda.ru/StaticContent/Photos/160525131253/160602184657/p_O.jpg'
+        },
+        {
+            id: 3,
+            name: 'Пюре',
+            description: 'Пюре из картофеля',
+            photoLink: 'http://v.img.com.ua/b/600x500/f/28/28b3d5cd38502f8075723e25c69ba28f.jpg'
+        },
+        {
+            id: 4,
+            name: 'Котлета по-киевски',
+            description: 'Котлета по-киевски готовится из отбитого куриного филе с начинкой из сливочного масла с зеленью и жарится в толстой панировке',
+            photoLink: 'https://i.ytimg.com/vi/C-nt0yqz-0g/maxresdefault.jpg'
+        },
+        {
+            id: 5,
+            name: 'Борщ',
+            description: 'Разновидность супа на основе свёклы, которая придаёт ему характерный красный цвет',
+            photoLink: 'https://lh3.googleusercontent.com/proxy/bEEPbZ4YNWHEMTpw-xbAgsRZo1bmDW1_M9qjRXVIILnv58pGa1VZbZGoYZuQLjiYH75Gij13u2rY7JqaW8dKrhRGZn9BApqLz0yBaVGNR8jyHOXVCOnHsO775UXS8WBGhqM'
+        },
+        {
+            id: 6,
+            name: 'Щи',
+            description: 'Разновидность заправочного супа, блюдо русской кухни.',
+            photoLink: 'https://blog-food.ru/images/site/recipes/first-dishes/0280-schi/07.jpg'
+        },
+        {
+            id: 7,
+            name: 'Салат оливье',
+            description: 'Популярный в России праздничный салат',
+            photoLink: 'https://img.povar.ru/main/71/a7/1b/ab/salat_quotolivequot_klassicheskii_s_kolbasoi-286788.JPG'
+        },
+        {
+            id: 8,
+            name: 'Морс ягодный',
+            description: 'Традиционный для русской кухни негазированный прохладительный напиток, ягодный сок, разбавленный водой и подслащенный',
+            photoLink: 'https://www.gastronom.ru/binfiles/images/20190225/b7860bc0.jpg'
+        },
+        {
+            id: 9,
+            name: 'Чай с мятой',
+            description: 'Водный настой из листьев мяты перечной, который не содержит кофеина',
+            photoLink: 'https://poleznenko.ru/wp-content/uploads/2013/04/mjatnyj-chaj-pohudenie.jpg'
+        },
+        {
+            id: 10,
+            name: 'Булочка с корицей',
+            description: 'Прекрасная выпечка к чаю, кофе, какао, молоку либо горячему шоколаду',
+            photoLink: 'https://beregifiguru.ru/Storage/Food/%D0%91%D1%83%D0%BB%D0%BE%D1%87%D0%BA%D0%B0-%D1%81-%D0%BA%D0%BE%D1%80%D0%B8%D1%86%D0%B5%D0%B9-187170.jpg'
+        }
+
+    ]
+
+    get() {
+        return this.dishes;
+    }
+
+    getById(id) {
+        return this.dishes.find(dish => dish.id === id);
+    }
+
+    getIndexById(id) {
+        return this.dishes.findIndex(dish => dish.id === id) + 1;
+    }
+
+    getIdToDish() {
+        const idToDish = new Map();
+        this.get().forEach(dish => idToDish.set(dish.id, dish));
+        return idToDish;
+    }
+}
+
---
+++ scripts/main-page.js
@@ -0,0 +1,297 @@
+const restaurantClient = new RestaurantClient('');
+const dishClient = new DishClientStub();
+const deliveryPrice = 250;
+let selectedRestaurant = {};
+const selectedDishIdToQuantity = new Map();
+let selectedPresent = false;
+let selectedGroupOrder = false;
+let randomPresentDishIdx = -1;
+
+const info = (item) => {
+    console.info(item);
+    return item;
+}
+
+const onSelectRestaurant = async (id) => {
+    const previousSelected = document.querySelector(`#restaurant-select-${selectedRestaurant.id}`);
+    if (previousSelected) {
+        previousSelected.disabled = false;
+        previousSelected.innerText = 'Выбрать';
+    }
+    selectedRestaurant = await restaurantClient.getById(id);
+    const currentSelected = document.querySelector(`#restaurant-select-${id}`);
+    currentSelected.disabled = true;
+    currentSelected.innerText = 'Выбрано';
+    renderMenu();
+};
+
+const convertResponseToTableBody = (resp) => {
+    return resp
+        .map(elem => {
+            const selected = elem.id === selectedRestaurant.id;
+            return `<tr>
+                <td>${elem.name}</td>
+                <td>${elem.typeObject}</td>
+                <td>${elem.admArea}</td>
+                <td>${elem.district}</td>
+                <td>${elem.address}</td>
+                <td>${elem.rate}</td>
+                <td>${elem.socialPrivileges === 1 ? 'Да' : 'Нет'}</td>
+                <td>
+                    <button ${selected ? 'disabled' : ''} class="btn btn-primary" id="restaurant-select-${elem.id}" type="button" onclick="onSelectRestaurant(${elem.id})">
+                    ${selected ? 'Выбрано' : 'Выбрать'}
+                    </button>
+                </td>
+            </tr>`
+        })
+        .join('');
+}
+
+const filter = (admArea, district, type, discount) => {
+    return (elem) => {
+        return elem && elem.typeObject && elem.admArea && elem.district && (elem.socialPrivileges > -1) &&
+            elem.typeObject.indexOf(type) !== -1 &&
+            elem.district.indexOf(district) !== -1 &&
+            elem.admArea.indexOf(admArea) !== -1 &&
+            (discount === 0 || elem.socialPrivileges === 1);
+    };
+}
+
+const onClickFind = async () => {
+    const admArea = document.querySelector('#adm-area').value;
+    const district = document.querySelector('#district').value;
+    const type = document.querySelector('#type').value;
+    const discount = document.querySelector('#discount').checked ? 1 : 0;
+
+
+    const tbody = await restaurantClient.get(0, 1, filter(admArea, district, type, discount))
+        .then(resp => (info(resp)))
+        .then(resp => (convertResponseToTableBody(resp)));
+    const element = document.querySelector('.restaurants');
+    element.innerHTML = `<table class="table pink-with-turquoise mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">Название</th>
+                    <th scope="col">Тип</th>
+                    <th scope="col">Округ</th>
+                    <th scope="col">Район</th>
+                    <th scope="col">Адрес</th>
+                    <th scope="col">Рейтинг</th>
+                    <th scope="col">Соц. скидка</th>
+                    <th scope="col">Действия</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${tbody}
+            </tbody>
+        </table>
+        <nav>
+            <ul class="pagination justify-content-center">
+                <li class="page-item disabled">
+                    <a class="page-link" tabindex="-1">Пред</a>
+                </li>
+                <li class="page-item"><a class="page-link">1</a></li>
+                <li class="page-item"><a class="page-link">2</a></li>
+                <li class="page-item"><a class="page-link">3</a></li>
+                <li class="page-item">
+                    <a class="page-link">След</a>
+                </li>
+            </ul>
+        </nav>`;
+};
+
+const renderMenu = async () => {
+    const menuHtml = dishClient.get()
+        .map((menu, idx) => {
+            const price = selectedRestaurant[`set_${idx + 1}`];
+            return `<div class="col-4">
+                <div class="pink-with-turquoise h-100">
+                    <div class="row">
+                        <div class="text-center col-12">
+                        <h5>${menu.name}</h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="text-center col-12">
+                            <img src="${menu.photoLink}" class="img-thumbnail" width="100px">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="text-center col-12">${menu.description}</div>
+                    </div>
+                    <div class="row">
+                        <div class="text-center col-12 font-weight-bold">
+                            ${price} RUR
+                        </div>
+                    </div>
+                    <div class="container mt-1">
+                         <div class="row align-self-end">
+                             <div class="col-lg-2"></div>
+                             <div class="col-lg-8 col-12">
+                                 <div class="input-group mb-3">
+                                     <div class="input-group-prepend">
+                                         <button class="btn btn-dark btn-sm" onclick="onChangeDishQuantity(-1, ${menu.id})"><i class="fa fa-minus"></i></button>
+                                     </div>
+                                     <input type="number" id="qty_input-${menu.id}" class="form-control form-control-sm" value="0" min="0">
+                                     <div class="input-group-prepend">
+                                         <button class="btn btn-dark btn-sm" onclick="onChangeDishQuantity(1, ${menu.id})"><i class="fa fa-plus"></i></button>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+            </div>`;
+        })
+        .reduce((prev, curr) => {
+            if (prev.length === 0 || prev[prev.length - 1].length % 3 === 0) {
+                prev.push([curr]);
+            } else {
+                prev[prev.length - 1].push(curr);
+            }
+            return prev;
+        }, [])
+        .map(arr => {
+            return `<div class="row mt-1 mb-1">${arr.join('')}</div>`
+        })
+        .join('');
+    const doc = document.querySelector('#menu-output');
+    doc.innerHTML = `<h3 class="text-center">Меню</h3>${menuHtml}`;
+};
+
+const onChangeDishQuantity = (value, id) => {
+    const inputElement = document.querySelector(`#qty_input-${id}`);
+    const newValue = parseInt(inputElement.value) + value;
+    if (newValue >= 0) {
+        inputElement.value = newValue;
+        selectedDishIdToQuantity.set(id, newValue);
+    } else {
+        selectedDishIdToQuantity.delete(id);
+    }
+    onOrderChange();
+}
+
+const computeOrderPrice = () => {
+    const dishIdToPrice = dishClient.get()
+        .reduce((prev, curr, idx) => {
+            prev.set(curr.id, selectedRestaurant[`set_${idx + 1}`]);
+            return prev;
+        }, new Map());
+
+    return Array.from(selectedDishIdToQuantity) // Array of arrays [[dishId, dishQuantity], ...]
+        .reduce((accumulator, currentValue) => {
+            const dishId = currentValue[0];
+            const dishQuantity = currentValue[1];
+            return accumulator + dishIdToPrice.get(dishId) * dishQuantity;
+        }, 0)
+}
+
+const onOrderChange = () => {
+    const orderPrice = computeOrderPrice();
+    document.querySelector('#price').innerText = `Итого: ${orderPrice} RUR`;
+    document.querySelector('[name="order-btn"]').disabled = orderPrice === 0;
+}
+
+const renderOrder = () => {
+    return Array.from(selectedDishIdToQuantity)
+        .map(element => {
+            const dish = dishClient.getById(element[0]);
+            const quantity = element[1];
+            const price = selectedRestaurant[`set_${dishClient.getIndexById(element[0])}`];
+            return `<div class="row">
+                        <div class="col-2"><img class="img-thumbnail" src="${dish.photoLink}" alt="${dish.name}"></div>
+                        <div class="col-4"><h5>${dish.name}</h5></div>     
+                        <div class="col-3">${price}×${quantity}</div>     
+                        <div class="col-3"><h5>${price * quantity} RUR</h5></div>     
+                    </div>`
+        })
+        .join('');
+};
+
+const renderAdditionalOpts = () => {
+    const presentHtml = selectedPresent
+        ? `<div class="row">
+                <div class="col-6">Хочу подарок</div>
+                <div class="col-6">${dishClient.get()[randomPresentDishIdx].name} ${selectedRestaurant['set_' + (randomPresentDishIdx + 1)]}×1 RUR</div>
+            </div>`
+        : '';
+
+    const groupOrderHtml = selectedGroupOrder
+        ? `<div class="row">
+                <div class="col-6">На компанию</div>
+                <div class="col-6">-${computeOrderPrice() * 0.5} RUR</div>
+            </div>`
+        : '';
+    return presentHtml + groupOrderHtml;
+};
+
+const renderRestaurant = () => {
+    const restaurant = selectedRestaurant;
+    return `<div class="row"><div class="col-6">Название</div><div class="col-6">${restaurant.name}</div></div>
+            <div class="row"><div class="col-6">Административный округ</div><div class="col-6">${restaurant.admArea}</div></div>
+            <div class="row"><div class="col-6">Район</div><div class="col-6">${restaurant.district}</div></div>
+            <div class="row"><div class="col-6">Адрес</div><div class="col-6">${restaurant.address}</div></div>
+            <div class="row"><div class="col-6">Рейтинг</div><div class="col-6">${restaurant.rate}</div></div>`;
+};
+const computeTotalPrice = () => {
+    return deliveryPrice + computeOrderPrice() *
+        (selectedGroupOrder ? 0.5 : 1) +
+        (selectedPresent ? selectedRestaurant[`set_${randomPresentDishIdx + 1}`] : 0);
+};
+
+const onClickOrder = () => {
+    if (selectedPresent) {
+        randomPresentDishIdx = Math.floor(Math.random() * 10);
+    }
+
+    if (selectedGroupOrder) {
+        selectedDishIdToQuantity.forEach((v, k) => selectedDishIdToQuantity.set(k, v * 5));
+    }
+
+    const renderedOrder = renderOrder();
+    const renderedAdditionalOpts = renderAdditionalOpts();
+    const renderedRestaurant = renderRestaurant();
+    const totalPrice = computeTotalPrice();
+
+    const modalBody = document.querySelector('.modal-body');
+    modalBody.innerHTML = `
+    <div class="container">
+        <h4>Ваш заказ</h4>
+        ${renderedOrder}
+    </div>
+    <div class="container">
+        <h4>Дополнительные опции</h4>
+        ${renderedAdditionalOpts}
+    </div>
+    <div class="container">
+        <h4>Информация о предприятии</h4>
+        ${renderedRestaurant}
+    </div>
+    <div class="container">
+        <h5>Доставка</h5>
+        <form>
+          <div class="form-group row">
+            <label for="delivery-address" class="col-sm-4 col-form-label">Адрес доставки</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="delivery-address">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="delivery-name" class="col-sm-4 col-form-label">ФИО получателя</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="delivery-name">
+            </div>
+          </div>
+        <div class="form-group row">
+            <label for="delivery-price" class="col-sm-4 col-form-label">Стоимость доставки</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="delivery-price" disabled value="${deliveryPrice} RUR">
+            </div>
+          </div>
+        </form>
+    </div>
+    <div class="container">
+        <h5>Итого: ${totalPrice} RUR</h5>
+    </div>
+`;
+}
